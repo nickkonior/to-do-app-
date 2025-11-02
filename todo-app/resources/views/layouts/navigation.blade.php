b0ea5b98de5f335e500cc3dad130245f786536c4
nickkonior <nikoskoni30m1@gmail.com>
2025-11-02 18:20:09 +0200

    dashoboard

--- todo-app/resources/views/layouts/navigation.blade.php
+++ todo-app/resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                    <a href="{{ route('dashboard') }}" class="text-xl font-bold text-gray-700">
+                        To-Do App
                     </a>
                 </div>
 
